a8c0445a3b2f858c95266f161b6d7b42ae90d8bf
Peter Cusack <cusackpeter@gmail.com>
2019-01-28 11:18:44 -0800

    adding fully functional session managment

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,9 +1,9 @@
 const  pgPool = require('../dbPool');
 const bcrypt = require('bcrypt')
+
 const dbErrorCatch = (error) => {
-	console.log("DB Error")
-	console.log(error)
-	return { error }
+	console.log("DB Error:", error)
+	throw error;
 	// switch(error.code){
 	// 	case 23505: //
 	// 		return {httpCode: 422, error}
@@ -11,20 +11,17 @@ const dbErrorCatch = (error) => {
 	// 		return {httpCode: 500,  error}
 	// }
 }
-
-const getUser = (username) => {
+const getUser = ({ username }) => {
 	return pgPool.query('\
 		SELECT * FROM app_user \
-		WHERE username = $1', [1],
-		(err, res) => {
-	})
+		WHERE username = $1', 
+		[username])
 	.catch(dbErrorCatch);
 }
 
 const createUser = ({username, email, password}) => {
 	return bcrypt.hash(password, 5)
 	.then((password) => {
-		console.log("::::::::", username, email, password)
 		return pgPool.query('\
 		INSERT INTO app_user(email, username,  password) \
 		VALUES ($1, $2, $3) \
@@ -34,20 +31,25 @@ const createUser = ({username, email, password}) => {
 	.catch(dbErrorCatch);
 }
 
-const validUserPass = ({ username, password }) => {
+const validUserPass = ({username, password}) => {
 	return pgPool.query('\
 		SELECT password FROM app_user \
-		WHERE username = $1', username)
-	.then(({rows}) => {
-		return bcrypt.compare(rows[0].password, password)
+		WHERE username = $1', 
+		[username])
+	.then(({ rows }) => {
+		if (rows.length){
+			return bcrypt.compare(password, rows[0].password)
+		} else {
+			return false
+		}
 	})
 	.catch(dbErrorCatch);
 }
 
-// These should be moved into a seperate file.. 
-// but all this will be on redis eventually
+// These should be moved into a seperate file.. //
+// but all this will be on redis eventually //
+
 const createSession = ({username, expires, session}) => {
-	console.log(expires)
 	return pgPool.query('\
 		INSERT INTO session(username, session_id) \
 		VALUES ($1, $2) \
@@ -56,17 +58,30 @@ const createSession = ({username, expires, session}) => {
 	.catch(dbErrorCatch);
 }
 
-const validateSession = ({sessionKey, username}) => {
+const deleteSession = ({ session }) => {
+	return pgPool.query('\
+		DELETE FROM session \
+		WHERE session_id = $1',
+		[session])
+	.catch(dbErrorCatch)
+}
+
+const validateSession = ({session, username}) => {
 	return pgPool.query('\
 		SELECT * from session \
-		WHERE session_key = $1 AND username = $2', 
-		sessionKey, userID)
-	.catch(dbErrorCatch);;
+		WHERE session_id = $1 AND username = $2', 
+		[session, username])
+	.then(({rows}) => {
+		return rows.length > 0;
+	})
+	.catch(dbErrorCatch);
 }
 
 module.exports = {
 	getUser,
 	createUser,
 	createSession,
-	validateSession
+	validateSession,
+	validUserPass,
+	deleteSession
 }
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,25 @@
 const db = require('../db/queries/users');
 const uuid = require('uuid/v4')
 const Moment = require('Moment')
-/* POST /users/ */
-// GET /users/username
+
+// GET /users/:username //
 const getUser = (req, res, next) => {
-	return db.getUser(req.params.username)
-	.then(dbRes => {
-		res.json(dbRes)
+	return db.getUser({username: req.params.username})
+	.then(({rows}) => {
+		let user = rows[0]
+		res.json({
+			user: {
+				username: user.username,
+				email: user.email
+			// more to come
+			}
+		})
 		return next()
 	})
 }
 
-/* POST /users/ */
-const createUser = ({params}, res, next) => {
+// POST /users/ body: {email, password, username}//
+const createUser = ({ params }, res, next) => {
 	if (!params.username || !params.email || !params.password){
 		res.json({error: 'Missing argument'})
 		res.status(400)
@@ -22,7 +29,6 @@ const createUser = ({params}, res, next) => {
 	.then(({error = null, rows}) => {
 		// invalid User or bad database request
 		if (error) {
-			console.log(error)
 			res.status(400)
 			res.json({ error: error.details })
 			return next()
@@ -30,29 +36,60 @@ const createUser = ({params}, res, next) => {
 		} else {
 			return createSession(params.username)
 			.then((entry) => {
-				console.log(entry)
 				res.json({session: entry.rows[0].session_id, user: rows[0]})
 				return next()
 			})
 		}
 	})
+	.catch(err => {
+		//  unique constraint violation
+		if (err.code == '23505'){
+			res.json({'Error': 'Email already exist'})
+			res.status(400)
+			return next()			
+		}
+		throw err;
+	})
 }
 
-/* POST /user/username/session */
-const login = (req, {username, session}, next) => {
-	return db.validUserPass(req.params)
-	.then( valid => {
+// POST /users/login //
+const login = ({ params }, res, next) => {
+	const {username, password} = params;
+	const responseJson = {};
+	return db.validUserPass({username, password})
+	.then(valid => {
 		if (valid) {
-			getUser(username)
-			return db.createSession((session) => {
-				res.json({session})
-				return next()
+			return db.getUser({username})
+			.then(({ rows }) => {
+				responseJson["user"] = {
+					username: rows[0].username,
+					email: rows[0].email
+				}
+				return createSession(username);
 			})
+			.then(({ rows }) => {
+				responseJson["session"] = rows[0].session_id;
+				res.json(responseJson)
+				return next();
+			});
 		} else {
 			res.json({error: "Username and Password not Valid"});
 			res.status(400);
 			return next();
 		}
+	})
+	.catch(err => {
+		throw err;
+	})
+}
+
+const logout = (req, res, next) => {
+	return db.deleteSession({ session: req.query.session })
+	.then(({ rowCount }) => {
+		if (rowCount == 1){
+			return res.json({logout: true})
+		}
+		return next();
 	})
 }
 
@@ -84,5 +121,5 @@ module.exports = {
 	createUser,
 	getUser,
 	login,
-//	logout
+	logout
 }
